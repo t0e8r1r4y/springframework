dbd23657755f3e021fb43b0644951dcadc579bbf
Terry <terryakishin0814@gmail.com>
2022-10-23 12:36:33 +0900

    first commit

---
+++ src/main/java/com/hodolog/hodollog/HodollogApplication.java
@@ -0,0 +1,13 @@
+package com.hodolog.hodollog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodollogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodollogApplication.class, args);
+	}
+
+}
