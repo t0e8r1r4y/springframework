c984397506382b1dbd27735f874d2f8ef44103ec
Terry <terryakishin0814@gmail.com>
2022-10-23 16:15:44 +0900

    alt + enter를 누르면 static method로 바꿀 수 있다.

--- src/test/java/com/hodolog/hodollog/controller/PostControllerTest.java
+++ src/test/java/com/hodolog/hodollog/controller/PostControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -69,7 +71,7 @@ class PostControllerTest {
     @DisplayName("/v1/posts4 요청시 test4을 출력한다.")
     void post4() throws Exception {
         mockMvc.perform(post("/v1/posts4")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"titme\": \"제목입니다.\", \"content\": \"내용입니다.\"}"))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
@@ -79,7 +81,7 @@ class PostControllerTest {
     @DisplayName("/v1/post4 요청 시 타이틀은 필수입니다.")
     void testPost4() throws Exception {
         mockMvc.perform(post("/v1/posts4")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"titme\": \"\", \"content\": \"내용입니다.\"}"))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
@@ -89,7 +91,7 @@ class PostControllerTest {
     @DisplayName("/v1/post5 요청 시 타이틀은 필수입니다. spring validation을 사용한 처리 방법입니다.")
     void post5() throws Exception {
         mockMvc.perform(post("/v1/posts5")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"titme\": \"\", \"content\": \"내용입니다.\"}"))
                 .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
@@ -111,7 +113,7 @@ class PostControllerTest {
     void post6() throws Exception {
         // 위와 다르게 본래 테스트 대상의 메서드에서 BindingResult result를 제거해줘야 한다.
         mockMvc.perform(post("/v1/posts6")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"titme\": \"\", \"content\": \"내용입니다.\"}"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
@@ -124,7 +126,7 @@ class PostControllerTest {
     void post7() throws Exception {
         // when
         mockMvc.perform(post("/v1/posts7")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"title\": \"제목임다.\", \"content\": \"내용입니다.\"}"))
                 .andExpect(status().isOk())
                 .andDo(print());
