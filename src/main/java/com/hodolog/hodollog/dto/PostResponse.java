b5fa956f6ed4240fff7b58006f81e8346901900b
Terry <terryakishin0814@gmail.com>
2022-10-23 20:29:20 +0900

    기능 수정

--- src/main/java/com/hodolog/hodollog/dto/PostResponse.java
+++ src/main/java/com/hodolog/hodollog/dto/PostResponse.java
@@ -12,7 +12,7 @@ public class PostResponse {
 
     public PostResponse(Post post) {
         this.id = post.getId();
-        this.title = post.getTitle();
+        this.title = post.getTitle().substring(0, Math.min(post.getTitle().length(), 10) ); // 요런 요구사항이 있으면 이것도 따로 테스트로 구현하는 것도 중요함
         this.content = post.getContent();
     }
 }
