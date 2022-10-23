a9f2fdee39db5f2d3558d73e6ac5e12fe2cb46b6
Terry <terryakishin0814@gmail.com>
2022-10-24 00:20:13 +0900

    Post Edit dto 추가

---
+++ src/main/java/com/hodolog/hodollog/dto/PostEdit.java
@@ -0,0 +1,26 @@
+package com.hodolog.hodollog.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@ToString
+public class PostEdit {
+
+    @NotBlank(message = "타이틀을 입력하세요.")
+    private String title;
+
+    @NotBlank(message = "콘텐츠를 입력해주세요.")
+    private String content;
+
+    @Builder
+    public PostEdit(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
